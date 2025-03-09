62141e269b8f7e3da06e7f0e09cb3bbd4de635c1
Henrique Papile <hpapile@gmail.com>
2025-03-09 13:09:13 -0300

    ajustes MovieCard

--- src/Components/Movies/MovieCard.jsx
+++ src/Components/Movies/MovieCard.jsx
@@ -1,8 +1,20 @@
 import { Link } from 'react-router-dom';
 import image from '../../assets/no-image.jpg';
+import { useEffect, useState } from 'react';
 
 const MovieCard = ({ id, poster_path, title, release_date }) => {
   const movieImage = 'https://image.tmdb.org/t/p/original' + poster_path;
+  const [releaseDate, setReleaseDate] = useState(null);
+
+  function formatDate(dataString) {
+    if (!dataString) return '';
+    const [year, month, day] = dataString.split('-');
+    return `${day}/${month}/${year}`;
+  }
+
+  useEffect(() => {
+    setReleaseDate(formatDate(release_date));
+  }, [setReleaseDate]);
 
   return (
     <div>
@@ -17,8 +29,8 @@ const MovieCard = ({ id, poster_path, title, release_date }) => {
           <figcaption className="font-bold text-base text-primary mt-2 overflow-hidden text-ellipsis whitespace-nowrap">
             {title}
           </figcaption>
-          <span className="font-bold text-sm text-secondary">
-            {release_date}
+          <span className="font-bold text-base text-secondary">
+            {releaseDate}
           </span>
         </figure>
       </Link>
